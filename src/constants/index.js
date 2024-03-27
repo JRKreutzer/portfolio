31a3923f2f40c7a13a7450c2ffca17982552055f
JRKreutzer <jose.kreutzer@gmail.com>
2024-03-26 22:11:38 -0300

    Pequena Alteração nos Depoimentos

--- src/constants/index.js
+++ src/constants/index.js
@@ -417,13 +417,13 @@ export const testimoniais = [
         id: 2,
         image: testimonial2Img,
         title: 'Harry Clinton',
-        description: 'A really good job, all aspects of the project were followed step by step and with good results. Muito pirocudo.'
+        description: 'A really good job, all aspects of the project were followed step by step and with good results.'
     },
     {
         id: 3,
         image: testimonial3Img,
         title: 'Sara Cill',
-        description: 'A really good job, all aspects of the project were followed step by step and with good results. Muito pirocudo.'
+        description: 'A really good job, all aspects of the project were followed step by step and with good results.'
     }
 ]
 
